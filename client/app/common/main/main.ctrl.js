0aeff428e27fbb6ead98e7af8c693216d48e01fd
Northwalker <northwalker@hotmail.com>
2017-03-30 01:55:36 +0800

    MainController bug fixed.

--- client/app/common/main/main.ctrl.js
+++ client/app/common/main/main.ctrl.js
@@ -6,8 +6,17 @@
     .controller('MainController', MainController);
 
   /* @ngInject */
-  function MainController() {
+  // MainController.$inject =['$scope'];
+  function MainController($scope) {
     var vm = this;
+    $scope.dayText = 'a good day.';
+
+    function init() {
+      console.log('Greeting from AWSK main controller initial function.');
+      $scope.dayText = new Date().getFullYear() + '-' + (new Date().getMonth() + 1) + '-' + new Date().getDate();
+
+    }
+    init();
 
   }
 
